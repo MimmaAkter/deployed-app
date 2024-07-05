29d8200509c86079570de7acf5270cf96d4eb00c
mimma-akter <developermernstack436@gmail.com>
2024-07-05 15:07:05 +0600

    config got backend link

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:5050' 
+      '/api':'https://deployed-app-server.vercel.app' 
     },
   },
   plugins: [react()],
